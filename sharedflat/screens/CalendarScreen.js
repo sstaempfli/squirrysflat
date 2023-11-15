e3533b9469950000315850dae59bc3fa33e792d3
Adam Suma <adam@civitronic.ro>
2023-11-15 16:33:53 +0100

    navigation complete for now

---
+++ sharedflat/screens/CalendarScreen.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function CalendarScreen() {
+    return (
+      <View>
+        <Text>CalendarScreen</Text>
+      </View>
+    );
+}
---
+++ sharedflat/screens/FinanceScreen.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function FinanceScreen() {
+    return (
+      <View>
+        <Text>FinanceScreen</Text>
+      </View>
+    );
+}
