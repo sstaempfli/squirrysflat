b34f494a1da8a7d6aaedbf02ff4c3c3c88c07d70
Adam Suma <adam@civitronic.ro>
2023-11-21 13:55:28 +0100

    state management initial

--- sharedflat/App.js
+++ sharedflat/App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,64 +7,58 @@ import GameScreen from './screens/GameScreen';
 import CalendarScreen from './screens/CalendarScreen';
 import FinanceScreen from './screens/FinanceScreen';
 import { FontAwesome, FontAwesome5 } from '@expo/vector-icons';
+import { TasksProvider } from './context/TasksContext';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName='Dashboard'
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <TasksProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName='Dashboard'
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-            switch(route.name){
-              case "Dashboard":
-                iconName = "home"
-                break
-              case "Finance":
-                iconName = "credit-card"
-                break
-              case "Calendar":
-                iconName = "calendar"
-                return  <FontAwesome name={iconName} color={color} size={size} />
-              case "Game":
-                iconName = "gamepad"
-                break
-              case "Tasks":
-                iconName = "tasks"
-                break
-            }
-            return <FontAwesome5 name={iconName} color={color} size={size} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-          tabBarActiveTintColor: "purple",
-          tabBarInactiveTintColor: "gray",
-          tabBarStyle: [
-            {
-              "display": "flex",
+              switch(route.name){
+                case "Dashboard":
+                  iconName = "home"
+                  break
+                case "Finance":
+                  iconName = "credit-card"
+                  break
+                case "Calendar":
+                  iconName = "calendar"
+                  return  <FontAwesome name={iconName} color={color} size={size} />
+                case "Game":
+                  iconName = "gamepad"
+                  break
+                case "Tasks":
+                  iconName = "tasks"
+                  break
+              }
+              return <FontAwesome5 name={iconName} color={color} size={size} />;
             },
-            null
-          ]
-        })}
-      >
-        <Tab.Screen name="Finance" component={FinanceScreen} />
-        <Tab.Screen name="Calendar" component={CalendarScreen} />
-        <Tab.Screen name="Dashboard" component={DashboardScreen} />
-        <Tab.Screen name="Tasks" component={TasksScreen} />
-        <Tab.Screen name="Game" component={GameScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>  
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+            tabBarActiveTintColor: "purple",
+            tabBarInactiveTintColor: "gray",
+            tabBarStyle: [
+              {
+                "display": "flex",
+              },
+              null
+            ]
+          })}
+        >
+          <Tab.Screen name="Finance" component={FinanceScreen} />
+          <Tab.Screen name="Calendar" component={CalendarScreen} />
+          <Tab.Screen name="Dashboard" component={DashboardScreen} />
+          <Tab.Screen name="Tasks" component={TasksScreen} />
+          <Tab.Screen name="Game" component={GameScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>  
+    </TasksProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
